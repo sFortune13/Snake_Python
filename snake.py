df5197c9378d41756166e70c2eafc1faa7aaabaa
sfortune021 <sfortune021@wethinkcode.co.za>
2022-11-07 13:51:35 +0200

    Skeleton code added

---
+++ snake.py
@@ -0,0 +1,47 @@
+import math
+import random
+import pygame 
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    rows = 0
+    w = 0
+    def __init__(self, start, dirx=1, diry=0,colour=(144, 238, 144)):
+        pass
+    def move(self, dirx, diry):
+        pass
+    def draw(self, surface, eyes=False):
+        pass
+
+class snake(object):
+    def __init__(self, colour, pos):
+        pass
+    def move(self):
+        pass
+    def reset(self, pos):
+        pass
+    def addCube(self):
+        pass
+    def draw(self, surface):
+        pass
+
+
+def drawMap(w, rows, surface):
+    pass
+
+
+def redrawWindow(surface):
+    pass
+
+
+def snakeFood(rows, items):
+    pass
+
+
+def message_box(subject, content):
+    pass
+
+
+def main():
+    pass
